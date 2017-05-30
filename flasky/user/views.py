686c0b3d1ad71adde9bdf1fdaef13d41a671c04d
wlingxiao <wlingxiao@outlook.com>
2017-05-30 12:42:10 +0800

    完善最后登录时间和注册时间分段查询后台代码

--- flasky/user/views.py
+++ flasky/user/views.py
@@ -2,7 +2,7 @@ from . import user
 from flasky.auth.models import User, db
 from flask import jsonify, render_template, request
 from sqlalchemy import and_
-from datetime import datetime
+import datetime
 
 
 @user.route('/load_all_user', methods=['GET'])
@@ -21,7 +21,26 @@ def load_all_user():
     if email:
         query_param.append(User.email == email)
 
-    users = User.query.filter(and_(*query_param)).limit(20).all()
+    datetime_min = datetime.time.min
+    datetime_max = datetime.time.max
+
+    if sign_up_time_start:
+        sign_up_time_start_datetime = datetime.datetime.strptime(sign_up_time_start, '%Y-%m-%d')
+        query_param.append(User.sign_up_time >= datetime.datetime.combine(sign_up_time_start_datetime, datetime_min))
+
+    if sign_up_time_end:
+        sign_up_time_end_datetime = datetime.datetime.strptime(sign_up_time_end, '%Y-%m-%d')
+        query_param.append(User.sign_up_time <= datetime.datetime.combine(sign_up_time_end_datetime, datetime_max))
+
+    if last_visit_time_start:
+        last_visit_time_start_datetime = datetime.datetime.strptime(last_visit_time_start, '%Y-%m-%d')
+        query_param.append(User.sign_up_time >= datetime.datetime.combine(last_visit_time_start_datetime, datetime_min))
+
+    if last_visit_time_end:
+        last_visit_time_end_datetime = datetime.datetime.strptime(last_visit_time_end, '%Y-%m-%d')
+        query_param.append(User.sign_up_time <= datetime.datetime.combine(last_visit_time_end_datetime, datetime_max))
+
+    users = User.query.filter(and_(*query_param)).order_by(User.last_visit_time.desc()).limit(20).all()
 
     filter_users_size = User.query.filter(and_(*query_param)).count()
 
@@ -30,9 +49,10 @@ def load_all_user():
         'msg': 'OK',
         'size': filter_users_size,
         'data': [
-            {'username': user.username, 'email': user.email, 'sign_up_time': user.sign_up_time.strftime('%Y-%m-%d'),
-             'last_visit_time': user.last_visit_time.strftime('%Y-%m-%d')} for
-            user in users]
+            {'username': per_user.username, 'email': per_user.email,
+             'sign_up_time': per_user.sign_up_time.strftime('%Y-%m-%d'),
+             'last_visit_time': per_user.last_visit_time.strftime('%Y-%m-%d')}
+            for per_user in users]
     }
 
     return jsonify(result)
