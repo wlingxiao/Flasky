10d6eedb9d9ec3c54fd746cb72291bf718299b75
wlingxiao <wlingxiao@outlook.com>
2017-06-17 00:23:11 +0800

    修复不能评论的bug

--- flasky/static/post/js/postController.js
+++ flasky/static/post/js/postController.js
@@ -55,8 +55,26 @@ define('postController', function () {
         };
 
         $scope.commitCommentToUser = function (commentId) {
-
-            var v = $scope;
-        }
+            $scope.submmitCommentToUser = function () {
+                $scope.commentToUserContent = $('#commentToUserContent').val();
+                if (!$scope.commentToUserContent) {
+                    console.log('no input');
+                    return;
+                }
+                var data = {content: $scope.commentToUserContent, 'post_id': 1};
+                $http({
+                    method: 'POST',
+                    url: '/post/comment_to_user/' + commentId,
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                    data: $httpParamSerializerJQLike(data)
+                }).then(function (response) {
+                    var data = response['data'];
+                    console.log(data)
+                })['catch'](function (response) {
+                    var a = response
+                })
+            }
+        };
     }
+
 });
