c7170abeba34a17262513d0c9d472895f34ab623
wlingxiao <wlingxiao@outlook.com>
2017-07-16 23:14:20 +0800

    update user resources restful api

--- flasky/api/User.py
+++
@@ -1,7 +0,0 @@
-from flask_restful import Resource
-from flask import jsonify
-
-
-class User(Resource):
-    def get(self, id):
-        return jsonify({'name': 'xiaoming', 'id': 1})
--- flasky/api/__init__.py
+++ flasky/api/__init__.py
@@ -1,7 +1,5 @@
 from flask import Blueprint
-from flask_restful import Api
-from .User import User
 
 api_blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
-api = Api(api_blueprint)
-api.add_resource(User, '/users/<int:id>')
+
+import flasky.api.UserApi
