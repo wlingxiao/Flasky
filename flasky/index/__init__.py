837363c8493966193fa6499b5dcaa55f4e517713
wlingxiao <wlingxiao@outlook.com>
2017-07-27 23:24:43 +0800

    remove index module

--- flasky/index/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-index = Blueprint('index', __name__)
-
-from . import views
--- flasky/index/views.py
+++
@@ -1,7 +0,0 @@
-from . import index
-from flask import render_template
-
-
-@index.route('/')
-def home():
-    return render_template('/index/home.html')
